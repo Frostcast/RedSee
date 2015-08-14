f5a1eafceed6075981098d09540ff6b39382448d
James Mortemore <james.mortemore@carthrottle.com>
2015-08-14 18:01:21 +0100

    Improved insert script, added progress bar and eta

--- scripts/insert.js
+++ scripts/insert.js
@@ -1,4 +1,8 @@
 var request = require('request')
+  , ProgressBar =  require('progress')
+  , async = require('async')
+  , byline = require('byline')
+  , fs = require('fs')
   , url = process.env.URL
 
 if (!url) {
@@ -6,110 +10,115 @@ if (!url) {
   process.exit(1)
 }
 
-var byline = require('byline')
-  , fs = require('fs')
-  , whiteInput = byline(fs.createReadStream(__dirname + '/../lists/words-whitelist.txt', { encoding: 'utf8' }))
-  , whiteCount = 0
-  , wordInc = 500
-  , words = []
+var whitelist = __dirname + '/../lists/words-whitelist.txt'
+  , blacklist = __dirname + '/../lists/words-blacklist.txt'
+  , ascii = __dirname + '/../lists/ascii-blacklist.txt'
 
-whiteInput.on('data', function (word) {
-  words.push(word)
-
-  if (words.length === wordInc) {
-    whiteInput.pause()
-    request(
-      { url: url + '/filter/word'
-      , method: 'POST'
-      , json: true
-      , headers: { 'content-type': 'application/json' }
-      , body: { msg: words , type: 'whitelist' }
-      }, function (error) {
-        if (error) {
-          console.error(error)
-        } else {
-          whiteCount+= words.length
-          words = []
-        }
-
-        whiteInput.resume()
-      })
+async.waterfall(
+  [ async.apply(getTotal, whitelist)
+  , function (total, cb) {
+    processFile(whitelist, 'whitelist', total, cb)
   }
-})
-
-whiteInput.on('end', function () {
-  if (words.length !== 0) {
-    request(
-      { url: url + '/filter/word'
-      , method: 'POST'
-      , json: true
-      , headers: { 'content-type': 'application/json' }
-      , body: { msg: words , type: 'whitelist' }
-      }, function (error) {
-        if (error) {
-          console.error(error)
-        } else {
-          whiteCount+= words.length
-        }
-
-        console.log('Added ' + whiteCount + ' words to whitelist')
-      })
-  } else {
-    console.log('Added ' + whiteCount + ' words to whitelist')
+  , async.apply(getTotal, blacklist)
+  , function (total, cb) {
+    processFile(blacklist, 'blacklist', total, cb)
   }
+  , async.apply(getTotal, ascii)
+  , function (total, cb) {
+    processFile(ascii, 'ascii', total, cb)
+  }
+  ]
+  , function (error) {
+    if (error) {
+      console.error(error)
+      process.exit(1)
+    }
 
-  var blackInput = byline(fs.createReadStream(__dirname + '/../lists/words-blacklist.txt', { encoding: 'utf8' }))
-   , blackCount = 0
+    process.exit()
+  }
+)
 
-  blackInput.on('data', function (word) {
-    blackInput.pause()
+function getTotal(filePath, callback) {
+  var total = 0
 
-    request(
-      { url: url + '/filter/word'
-      , method: 'POST'
-      , json: true
-      , headers: { 'content-type': 'application/json' }
-      , body: { msg: [ word ], type: 'blacklist' }
-      }, function (error) {
-        if (error) {
-          console.error(error)
-        } else {
-          blackCount++
-        }
+  // TODO Handle error
+  byline(fs.createReadStream(filePath, { encoding: 'utf8' }))
+    .on('data', function () {
+      total++
+    }).on('end', function () {
+      callback(null, total)
+    })
+}
 
-        blackInput.resume()
-      })
-  })
+function processFile(filePath, type, total, callback) {
+  var wordInc = 5
+    , requestCount = 0
+    , words = []
+    , totalRequests = 10
+    , bar = new ProgressBar('inserting :total ' + type + ' [:bar] :percent :etas'
+    , { complete: '='
+      , incomplete: ' '
+      , width: 50
+      , total: total
+      }
+    )
+    , stream = byline(fs.createReadStream(filePath, { encoding: 'utf8' }))
 
-  blackInput.on('end', function () {
-    console.log('Added ' + blackCount + ' words to blacklist')
+  stream.on('data', function (word) {
+    words.push(word)
 
-    var asciiInput = byline(fs.createReadStream(__dirname + '/../lists/ascii-blacklist.txt', { encoding: 'utf8' }))
-      , asciiCount = 0
+    if (words.length === wordInc) {
+      requestCount++
 
-    asciiInput.on('data', function (word) {
-      asciiInput.pause()
+      if (requestCount >= totalRequests) stream.pause()
 
       request(
-        { url: url + '/filter/ascii'
+        { url: url + '/filter/' + (type !== 'ascii' ? 'word' : 'ascii')
         , method: 'POST'
         , json: true
         , headers: { 'content-type': 'application/json' }
-        , body: { msg: [ word ] }
-        }, function (error) {
+        , body: { msg: words , type: type }
+        }
+        , function (error) {
           if (error) {
             console.error(error)
           } else {
-            asciiCount++
+            bar.tick(wordInc)
           }
 
-          asciiInput.resume()
+          requestCount--
+
+          if (requestCount === 0) {
+            requestCount = 0;
+            stream.resume()
+          }
         })
-    })
 
-    asciiInput.on('end', function () {
-      console.log('Added ' + asciiCount + ' ascii to blacklist')
-    })
-
+      words = []
+    }
   })
-})
+
+  stream.on('end', function () {
+    if (words.length !== 0) {
+      request(
+        { url: url + '/filter/' + (type !== 'ascii' ? 'word' : 'ascii')
+        , method: 'POST'
+        , json: true
+        , headers: { 'content-type': 'application/json' }
+        , body: { msg: words , type: type }
+        }
+        , function (error) {
+          if (error) return callback(error)
+
+          bar.tick(words.length)
+
+          console.log('Added ' + total + ' words to', type)
+
+          callback()
+        })
+    } else {
+      console.log('Added ' + total + ' words to', type)
+      callback()
+    }
+  })
+}
