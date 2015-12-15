7353597202be6dc5fb2017923b280436fa6d8222
James Mortemore <jamesmortemore@gmail.com>
2015-12-15 19:22:58 +0000

    Improved whitelist words

--- scripts/builder.js
+++ scripts/builder.js
@@ -2,12 +2,15 @@ var fs = require('fs')
   , byline = require('byline')
   , filter = require('redsee-filter')
   , _ = require('lodash')
-  , input = byline(fs.createReadStream('/usr/share/dict/words', { encoding: 'utf8' }))
-  , output = []
+  , request = require('request')
+  , input = byline(request('https://raw.githubusercontent.com/atebits/Words/master/Words/en.txt'))
+  , output = require('../lists/additional-whitelist-words.json')
   , blacklistedWords = require('../lists/storage/words-blacklist.json')
 
 input
-  .on('data', function (word) {
+  .on('data', function (buff) {
+    var word = buff.toString()
+
     if (blacklistedWords.indexOf(word.toLowerCase()) !== -1) return
     filter.normalise(null, null, word, function (error, normalised) {
       if (error) console.error(error)
